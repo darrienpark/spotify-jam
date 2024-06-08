a671eb925a01019e4fdf9229a6c2d65ce478580c
Darrien Park <darrienpark@Darriens-MacBook-Pro.local>
2024-06-07 20:17:25 -0400

    feat: Recreated spotify web player SDK example in project

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/auth/:path*",
+        destination: "http://localhost:3001/auth/:path*", // Proxy to Backend
+      },
+    ];
+  },
+  reactStrictMode: false,
+};
 
 export default nextConfig;
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -3,15 +3,27 @@
 import { ChangeEvent, useEffect, useState } from "react";
 import { io } from "socket.io-client";
 
+import Login from "@/components/login";
+import WebPlayback from "@/components/web-playback";
+
 const socket = io("http://localhost:3001");
 
 export default function Home() {
   const [input, setInput] = useState("");
+  const [token, setToken] = useState("");
 
   useEffect(() => {
     socket.on("input", (message: string) => {
       setInput(message);
     });
+
+    async function getToken() {
+      const response = await fetch("/auth/token");
+      const json = await response.json();
+      setToken(json.access_token);
+    }
+
+    getToken();
   }, []);
 
   function changeHandler(e: ChangeEvent<HTMLInputElement>): void {
@@ -24,6 +36,8 @@ export default function Home() {
       <header></header>
       <main>
         <input value={input} onChange={changeHandler} />
+
+        {token === "" ? <Login /> : <WebPlayback token={token} />}
       </main>
     </>
   );
